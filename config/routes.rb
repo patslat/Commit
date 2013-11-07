564e03734922fd3170764480ec143be9c9d5947b
Patrick Slattery <slattery.patrickj@gmail.com>
2013-11-07 10:45:43 -0800

    add basic api layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Commit::Application.routes.draw do
   root :to => 'root#root'
   get 'welcome' => 'root#welcome'
-  resource :session, :only => ['create', 'destroy', 'new']
+  resource :session, :only => [:create, :destroy, :new]
   resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :goals do
+        resources :commits, :only => [:create, :destroy]
+      end
+    end
+  end
 end
