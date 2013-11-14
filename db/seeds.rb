28f922bd27916c8b8f487466ac8481e8e2a648eb
Patrick Slattery <slattery.patrickj@gmail.com>
2013-11-14 15:28:21 -0800

    add some seeds and small amount of css

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,41 @@ g = u.goals.create!(
     maximum: "Build an app"
   }
 )
+
+steps = (DateTime.now - 100.days..DateTime.now).map do |date|
+  next if (rand * 2).to_i == 0
+  Step.new({
+    date: date,
+    work_done: [:maximum, :sufficient, :minimum].sample,
+    description: 'did stuff'
+  })
+end.compact!
+
+g = u.goals.create!(
+  description: "Work out every day",
+  steps: steps,
+  daily_goal: {
+    minimum: "Do some push-ups",
+    sufficient: "Run a mile or lift some weights",
+    maximum: "Go to the gym for an hour"
+  }
+)
+
+steps = (DateTime.now - 100.days..DateTime.now).map do |date|
+  next if (rand * 2).to_i == 0
+  Step.new({
+    date: date,
+    work_done: [:maximum, :sufficient, :minimum].sample,
+    description: 'did stuff'
+  })
+end.compact!
+
+g = u.goals.create!(
+  description: "Clean some stuff",
+  steps: steps,
+  daily_goal: {
+    minimum: "Put something away",
+    sufficient: "Fold some clothes / Do some dishes",
+    maximum: "Fold all of the clothes / Do all of the Dishes"
+  }
+)
